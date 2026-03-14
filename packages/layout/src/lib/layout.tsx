88d53532c8f3906fcfb6f9826c4099989a1e241a
augustbrown <august.brown@24g.com>
2026-03-14 18:37:00 -0400

    initializing a layout package

---
+++ packages/layout/src/lib/layout.tsx
@@ -0,0 +1,11 @@
+import styles from './layout.module.css';
+
+export function InithiumLayout() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to InithiumLayout!</h1>
+    </div>
+  );
+}
+
+export default InithiumLayout;
