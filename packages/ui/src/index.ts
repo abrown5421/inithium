469a6cde10350b69cfadd3d9f974d633fe3f9bca
augustbrown <august.brown@24g.com>
2026-04-14 10:51:53 -0400

    implementing a class safelist to ensure tailwinds JIT engine doesnt purge our custom theme classes

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,5 @@
 import './lib/theme/theme.css';
 import './lib/theme/fonts.css';
-export * from './lib/theme/tokens';
 export * from './lib/components/button'
 export * from './lib/components/text'
 export * from './lib/components/box'
